e1bcc19dc84bb9446531b2c5b5558bed6268f9a9
Andrea Giammarchi <andrea.giammarchi@mailonline.co.uk>
2016-10-25 16:18:43 +0100

    ooops, forgo to ssign the method

--- index.js
+++ index.js
@@ -278,8 +278,7 @@
     // otherwise replace global console methods
     for (i = 0; i < methods.length; i++) {
       key = methods[i];
-      overwrite = console[key];
-      if (!overwrite.raw) {
+      if (!console[key].raw) {
         console[key] = consolemd[key];
       }
     }
